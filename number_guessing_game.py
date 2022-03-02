5595309b379ba6038de1a805458a23a9f86f9fd3
sungcc <samooies@yahoo.com.hk>
2022-03-02 14:44:13 +0800

    update a life range aand valid response

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -19,19 +19,35 @@ def computer_guess(x):
     low = 1
     high = x
     feedback = ''
+    life = 5
+    print('Welcome to my number guessing game, u have 5 lifes to guess to right number')
 
     while feedback != 'c':
         if low != high:
             guess = random.randint(low, high)
         else: 
-            guess = low
+            guess = low    
         feedback = input(f"Is {guess} too high (H), or too low (L), or correct (C)?").lower()
-        if feedback == 'h':
+
+        if life == 0:
+            print('You ran out of life!')
+            break
+
+        elif feedback == 'h':
             high = guess - 1
+            life = life - 1
+            print(f"You have {life} life left")
         elif feedback == 'l':
             low = guess + 1
+            life = life -1 
+            print(f"You have {life} life left")
+        elif feedback == 'c':
+            print(f"yay The computer guessed your number, {guess}, correctly.")
         else:
-            print("Please enter a correct response")
+            print("please enter a valid response")
+            
+
+
         
-    print(f"yay The computer guessed your number, {guess}, correctly.")
+    
 
